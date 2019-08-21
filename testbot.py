e8291467aef2f89e71b94f1c951acfacb4d8be1d
J-Pell <badgfrng@gmail.com>
2019-08-21 01:37:44 -0400

    Initial Commit

---
+++ testbot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.utils import get
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+#@client.event
+#async def on_message(message):
+ #   if message.author == client.user:
+  #      return
+
+   # if message.content.startswith('$hello'):
+    #    await message.channel.send('Hello!')
+        
+    #channelID = message.channel.id   
+    #channel = client.get_channel(channelID)
+    #await channel.send(message.content)
+
+@client.event
+async def on_message(message):
+    # gets the guild
+    guild = message.guild
+    # user uses !Roles command to print a list of available roles on the server
+    if message.content.startswith('!Roles'): 
+        role_id_list = [598882204843769856, 598882290759892997]
+        
+        for role_id in role_id_list:
+            role = get(guild.roles, id = role_id)
+            channelID = message.channel.id   
+            channel = client.get_channel(channelID)
+            await channel.send(role, role.color)
+
+        
+
+
+client.run('NTk2MjIyODM1NDUwNzA3OTc4.XTaldQ.2wCOJyY8wlzGlgp1MZACn_ggQVw') 
+
+
+
+# [598882204843769856, 598882290759892997]
