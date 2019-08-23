45d70fd4363aaf3980a425928e7eb091747154bc
J-Pell <badgfrng@gmail.com>
2019-08-23 01:11:39 -0400

    broken @client in add-role function

--- testbot.py
+++ testbot.py
@@ -1,9 +1,13 @@
 from SecretKey import *
 import discord
+from discord.ext import commands
 from discord.utils import get
+import discord.utils
+
 
 
 client = discord.Client()
+
 
 
 @client.event
@@ -36,7 +40,35 @@ async def on_message(message):
             channel = client.get_channel(channelID)
             await channel.send(role, role.color)
 
-        
+
+# arg1 is role name and arg2 is role color
+# user types $CreateRole to trigger
+# user needs to add "" around any role that is more than 1 word
+# need manage roles premission -- just give admin
+
+
+@client.command(pass_context = True)
+async def CreateRole(ctx,arg1):
+    RoleName = arg1
+    #RoleColor = arg2
+    # gets the author of the message
+    author = ctx.message.author
+    # has the server create a role within the server
+    await author.create_role(author.guild, name = RoleName)
+    # 
+    role = discord.utils.get(author.guild.roles, name = RoleName)
+    # adds the role to the user
+    await author.add_roles(role)
+    
+
+
+
+
+
+
+
+# enter a comand to create a role and assign it a hex value
+# both are user inputs       
 #hello james
 
 client.run(secret) 
